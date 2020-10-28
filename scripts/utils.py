cc84f54c23c3f4b602b6c87c60de62910ce1848c
Philip Hartout <philip.hartout@protonmail.com>
2020-10-28 10:17:12 +0100

    :pencil: docs improvement

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,7 +7,6 @@ Utils module for the functions used in the exploring/ directory of this project.
 
 TODO:
     - add docstrings
-    - remove unused imports
     - make architectural distinction between plottling utils vs. non-plotting utils
     - see how tracing to patient can be implemented based on indices of matrix
 """
@@ -50,71 +49,6 @@ HOMOLOGY_DIMENSIONS = (0, 1, 2)
 DOTENV_KEY2VAL = dotenv.dotenv_values()
 N_JOBS = 6  # Set number of workers when parallel processing is useful.
 
-
-def make_3d_scatterplot(point_cloud, title):
-    df = pd.DataFrame(point_cloud).rename(columns={0: "x", 1: "y", 2: "z"})
-
-    x, y, z = df["x"].values, df["y"].values, df["z"].values
-    fig = go.Figure(
-        data=[
-            go.Scatter3d(
-                x=x,
-                y=y,
-                z=z,
-                mode="markers",
-                marker=dict(
-                    size=5,
-                    color=x,  # set color to an array/list of desired values
-                    colorscale="Viridis",  # choose a colorscale
-                    opacity=0.8,
-                ),
-            )
-        ]
-    )
-
-    # tight layout
-    fig.update_layout(margin=dict(l=0, r=0, b=0), title=title)
-    fig.show()
-
-
-def multi_slice_viewer(volume):
-    remove_keymap_conflicts({"j", "k"})
-    fig, ax = plt.subplots()
-    ax.volume = volume
-    ax.index = volume.shape[0] // 2
-    ax.imshow(volume[ax.index])
-    fig.canvas.mpl_connect("key_press_event", process_key)
-
-
-def process_key(event):
-    fig = event.canvas.figure
-    ax = fig.axes[0]
-    if event.key == "j":
-        previous_slice(ax)
-    elif event.key == "k":
-        next_slice(ax)
-    fig.canvas.draw()
-
-
-def previous_slice(ax):
-    volume = ax.volume
-    ax.index = (ax.index - 1) % volume.shape[0]  # wrap around using %
-    ax.images[0].set_array(volume[ax.index])
-
-
-def next_slice(ax):
-    volume = ax.volume
-    ax.index = (ax.index + 1) % volume.shape[0]
-    ax.images[0].set_array(volume[ax.index])
-
-
-def remove_keymap_conflicts(new_keys_set):
-    for distanceprop in plt.rcParams:
-        if prop.startswith("keymap."):
-            keys = plt.rcParams[prop]
-            remove_list = set(keys) & new_keys_set
-            for key in remove_list:
-                keys.remove(key)
 
 
 def prepare_image(directory, threshold):
@@ -274,28 +208,6 @@ def compute_distance_matrix(
     return X_distance
 
 
-def plot_distance_matrix(
-    X_distance, title=None, file_prefix=None,
-):
-
-    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8, 16))
-    plt.suptitle(title)
-    for i, ax in enumerate(axes.flat):
-        im = ax.imshow(X_distance[:, :, i], cmap="Blues")
-
-    fig.subplots_adjust(right=0.8)
-    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-    fig.colorbar(im, cax=cbar_ax)
-
-    plt.savefig(
-        DOTENV_KEY2VAL["GEN_FIGURES_DIR"]
-        + file_prefix
-        + "_distance_matrix.png",
-    )
-
-    return X_distance
-
-
 def evaluate_distance_functions(
     diagrams,
     list_of_distance_functions,
@@ -393,30 +305,6 @@ def get_distance_vectors_from_matrices(distance_matrices):
         distance_vectors.append(vector_for_distance_metric)
     return distance_vectors
 
-
-def compute_distplot(vectors, group_labels, title=None):
-    """list_of_distance_vectors must be structured in such a way that each list
-    of vectors can be plotted individually for each distance."""
-    fig = ff.create_distplot(
-        [np.log1p(vector) for vector in vectors], group_labels, bin_size=0.1
-    ).update_layout(title=title + " log transformed")
-    fig.show()
-    fig.write_html(
-        DOTENV_KEY2VAL["GEN_FIGURES_DIR"]
-        + "distplot_"
-        + title
-        + "_distance_vectors_log_transformed.html"
-    )
-    fig = ff.create_distplot(
-        [vector for vector in vectors], group_labels, bin_size=0.1
-    ).update_layout(title=title + " not log transformed")
-    fig.show()
-    fig.write_html(
-        DOTENV_KEY2VAL["GEN_FIGURES_DIR"]
-        + "distplot_"
-        + title
-        + "_distance_vectors_not_transformed.html"
-    )
 
 
 def plot_density_plots(metric):

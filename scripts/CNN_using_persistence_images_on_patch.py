fcb7f207dd7e8d95fe04d8321e87832fc25edb2a
Philip Hartout <philip.hartout@protonmail.com>
2020-11-24 12:34:37 +0100

    :art: remove unused code and comment

--- scripts/CNN_using_persistence_images_on_patch.py
+++ scripts/CNN_using_persistence_images_on_patch.py
@@ -45,146 +45,6 @@ print("DEVICE INFO")
 DOTENV_KEY2VAL = dotenv.dotenv_values()
 tf.random.set_seed(42)
 N_BINS = 100
-# physical_devices = tf.config.experimental.list_physical_devices('GPU')
-# tf.config.experimental.set_memory_growth(physical_devices[0], True)
-# gpus = tf.config.experimental.list_physical_devices("GPU")
-# if gpus:
-#     try:
-#         # Currently, memory growth needs to be the same across GPUs
-#         for gpu in gpus:
-#             tf.config.experimental.set_memory_growth(gpu, True)
-#         logical_gpus = tf.config.experimental.list_logical_devices("GPU")
-#         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-#     except RuntimeError as e:
-#         # Memory growth must be set before GPUs have been initialized
-#         print(e)
-
-
-# partition = {}
-# partition["validation"] = list(take(100, image_labels.items()).keys())
-# partition["train"] = [
-#     x for x in image_labels if x not in partition["validation"]
-# ]
-#
-# batch_size = 64
-#
-#
-# def create_batch(lst, size):
-#     batch_list = [
-#         lst[i * size : (i + 1) * size]
-#         for i in range((len(lst) + size - 1) // size)
-#     ]
-#     del batch_list[-1]
-#     return batch_list
-#
-#
-# partition["train"] = create_batch(partition["train"], batch_size)
-# partition["validation"] = create_batch(partition["validation"], batch_size)
-#
-#
-# def train_gen():
-#     for batch in partition["train"]:
-#         X = np.empty((batch_size, 100, 100, 3))
-#         y = np.empty((batch_size, 1))
-#         Xs = []
-#         ys = []
-#         for image in batch:
-#             Xs.append(
-#                 np.load(persistence_image_location + image).reshape(
-#                     1, 100, 100, 3
-#                 )
-#             )
-#             ys.append(image_labels[image])
-#
-#         yield np.vstack(Xs), np.vstack(ys)
-#
-#
-# def val_gen():
-#     for batch in partition["validation"]:
-#         X = np.empty((batch_size, 100, 100, 3))
-#         y = np.empty((batch_size, 1))
-#         Xs = []
-#         ys = []
-#         for image in batch:
-#             Xs.append(
-#                 np.load(persistence_image_location + image).reshape(
-#                     1, 100, 100, 3
-#                 )
-#             )
-#             ys.append(image_labels[image])
-#
-#         yield np.vstack(Xs), np.vstack(ys)
-
-
-################################################################################
-#  Classes
-################################################################################
-
-
-# class DataGenerator(keras.utils.Sequence):
-#     "Generates data for Keras"
-#     """
-#     https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
-#     """
-#
-#     def __init__(
-#         self,
-#         list_IDs,
-#         labels,
-#         batch_size=32,
-#         dim=(100, 100, 3),
-#         n_classes=2,
-#         shuffle=True,
-#     ):
-#         "Initialization"
-#         self.dim = dim
-#         self.batch_size = batch_size
-#         self.labels = labels
-#         self.list_IDs = list_IDs
-#         self.n_classes = n_classes
-#         self.shuffle = shuffle
-#         self.on_epoch_end()
-#
-#     def __len__(self):
-#         "Denotes the number of batches per epoch"
-#         return int(np.floor(len(self.list_IDs) / self.batch_size))
-#
-#     def __getitem__(self, index):
-#         "Generate one batch of data"
-#         # Generate indexes of the batch
-#         indexes = self.indexes[
-#             index * self.batch_size : (index + 1) * self.batch_size
-#         ]
-#
-#         # Find list of IDs
-#         list_IDs_temp = [self.list_IDs[k] for k in indexes]
-#
-#         # Generate data
-#         X, y = self.__data_generation(list_IDs_temp)
-#
-#         yield X, y
-#
-#     def on_epoch_end(self):
-#         "Updates indexes after each epoch"
-#         self.indexes = np.arange(len(self.list_IDs))
-#         if self.shuffle == True:
-#             np.random.shuffle(self.indexes)
-#
-#     def __data_generation(self, list_IDs_temp):
-#         "Generates data containing batch_size samples"  # X : (n_samples, *dim, n_channels)
-#         # Initialization
-#         X = np.empty((self.batch_size, *self.dim))
-#         y = np.empty((self.batch_size), dtype=int)
-#
-#         # Generate data
-#         for i, ID in enumerate(list_IDs_temp):
-#             # Store sample
-#             X[i,] = np.load(persistence_image_location + ID).reshape(100, 100, 3)
-#             # Store class
-#             y[i] = self.labels[ID]
-#
-#         return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
-#
 
 ################################################################################
 #  Functions
@@ -289,168 +149,116 @@ def main():
     ############################################################################
     #  Data loading and processing
     ############################################################################
-
-    # First, we load relevant images (only baseline image for now)
-    # (
-    #     cn_patients,
-    #     mci_patients,
-    #     ad_patients,
-    # ) = utils.get_all_available_diagnoses(diagnosis_json)
-
-    # image_names = cn_patients + ad_patients
-    # raw_labels = np.array(
-    #     [0 for i in range(len(cn_patients))]  # Labels for CN is 0
-    #     + [1 for i in range(len(ad_patients))]  # Labels for AD is 1
-    # )
-    # ratio_cn_ad = np.bincount(raw_labels)[1] / np.bincount(raw_labels)[0]
-    # print(f"Ratio of AD/CN patients is {ratio_cn_ad}")
-
-    # image_labels = {}
-    # for i, j in zip(image_names, list(raw_labels)):
-    #     image_labels[i] = j
     inits = 3
     partitions, labels = get_partitions(partitions_location)
     histories = []
-    # for partition, label in zip(partitions, labels):
-    #     for i in range(inits):
-    # Make sure there aren't the same patients in train and test
-    X_train_lst = []
-    y_train_lst = []
-    for image in partitions[0]["train"]:
-        X_train_lst.append(
-            np.load(persistence_image_location + image + ".npy")
-        )
-        y_train_lst.append(labels[0][image])
+    for partition, label in zip(partitions, labels):
+        for i in range(inits):
+            # Make sure there aren't the same patients in train and test
+            X_train_lst = []
+            y_train_lst = []
+            for image in partition["train"]:
+                X_train_lst.append(
+                    np.load(persistence_image_location + image + ".npy")
+                )
+                y_train_lst.append(label[image])
 
-        X_train, y_train = (
-            np.stack(X_train_lst, axis=0).reshape(
-                len(X_train_lst), N_BINS, N_BINS, 3
-            ),
-            np.vstack(y_train_lst),
-        )
-    X_val_lst = []
-    y_val_lst = []
-    for image in partitions[0]["validation"]:
-        X_val_lst.append(
-            np.load(persistence_image_location + image + ".npy")
-        )
-        y_val_lst.append(labels[0][image])
+                X_train, y_train = (
+                    np.stack(X_train_lst, axis=0).reshape(
+                        len(X_train_lst), N_BINS, N_BINS, 3
+                    ),
+                    np.vstack(y_train_lst),
+                )
+            X_val_lst = []
+            y_val_lst = []
+            for image in partition["validation"]:
+                X_val_lst.append(
+                    np.load(persistence_image_location + image + ".npy")
+                )
+                y_val_lst.append(label[image])
 
-        X_val, y_val = (
-            np.stack(X_val_lst, axis=0).reshape(
-                len(X_val_lst), N_BINS, N_BINS, 3
-            ),
-            np.vstack(y_val_lst),
-        )
+                X_val, y_val = (
+                    np.stack(X_val_lst, axis=0).reshape(
+                        len(X_val_lst), N_BINS, N_BINS, 3
+                    ),
+                    np.vstack(y_val_lst),
+                )
 
-    # group_kfold = GroupKFold(n_splits=2)
+            ####################################################################
+            #  Model definition
+            ####################################################################
 
-    # groups = list(map(lambda x: str(x.split("-")[1:2][0]), image_labels.keys()))
-    # mapping = {}
-    # for i, patient in enumerate(set(groups)):
-    #     mapping[patient] = i
-    # groups = [mapping[patient] for patient in groups]
+            model = make_model(input_shape=(N_BINS, N_BINS, 3))
+            tf.keras.utils.plot_model(
+                model,
+                to_file="model.png",
+                show_shapes=True,
+                show_layer_names=True,
+                rankdir="TB",
+                expand_nested=True,
+                dpi=96,
+            )
 
-    # for train_index, test_index in group_kfold.split(
-    #     X, y, groups
-    # ):
-    #     X_train, X_test = X[train_index,:,:,:], X[test_index,:,:,:]
-    #     y_train, y_test = y[train_index], y[test_index]
+            ####################################################################
+            #  Model training
+            ####################################################################
 
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     ,
-    #     ,
-    #     test_size=100,
-    #     random_state=42,
-    # )
-    # list(training_generator.take(2).as_numpy_iterator())
+            epochs = 100
 
-    # validation_generator = DataGenerator(
-    #     partition["validation"], image_labels, **params
-    # )
+            tensorboard_logs = "logs/fit"
+            if os.path.exists(tensorboard_logs):
+                shutil.rmtree(tensorboard_logs)
 
-    # next(train_data_generator)
-    ############################################################################
-    #  Model definition
-    ############################################################################
-
-    model = make_model(input_shape=(N_BINS, N_BINS, 3))
-    tf.keras.utils.plot_model(
-        model,
-        to_file="model.png",
-        show_shapes=True,
-        show_layer_names=True,
-        rankdir="TB",
-        expand_nested=True,
-        dpi=96,
-    )
-
-    ############################################################################
-    #  Model training
-    ############################################################################
-
-    epochs = 100
-
-    tensorboard_logs = "logs/fit"
-    if os.path.exists(tensorboard_logs):
-        shutil.rmtree(tensorboard_logs)
-
-    log_dir = "logs/fit/" + datetime.datetime.now().strftime(
-        "%Y%m%d-%H%M%S"
-    )
-    callbacks = [
-        # keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
-        tf.keras.callbacks.TensorBoard(
-            log_dir=log_dir, histogram_freq=1
-        ),
-        tf.keras.callbacks.EarlyStopping(
-            monitor="val_loss",
-            min_delta=0.00001,
-            patience=10,
-            verbose=0,
-            mode="auto",
-            baseline=None,
-            restore_best_weights=True,
-        ),
-    ]
-    lr = keras.optimizers.schedules.ExponentialDecay(
-        0.01, decay_steps=30, decay_rate=0.6, staircase=True
-    )
-    model.compile(
-        optimizer=keras.optimizers.Adam(
-            learning_rate=lr,
-            beta_1=0.9,
-            beta_2=0.999,
-            epsilon=1e-07,
-            amsgrad=False,
-        ),
-        loss="binary_crossentropy",
-        metrics=[
-            keras.metrics.BinaryAccuracy(name="accuracy"),
-            # keras.metrics.TruePositives(name="tp"),
-            # keras.metrics.FalsePositives(name="fp"),
-            # keras.metrics.TrueNegatives(name="tn"),
-            # keras.metrics.FalseNegatives(name="fn"),
-            keras.metrics.Precision(name="precision"),
-            keras.metrics.Recall(name="recall"),
-            keras.metrics.AUC(name="auc"),
-        ],
-        # run_eagerly=True,
-    )
-    history = model.fit(
-        X_train,
-        y_train,
-        epochs=epochs,
-        callbacks=callbacks,
-        batch_size=16,
-        validation_data=(X_val, y_val),
-    )
-    histories.append(history)
+            log_dir = "logs/fit/" + datetime.datetime.now().strftime(
+                "%Y%m%d-%H%M%S"
+            )
+            callbacks = [
+                tf.keras.callbacks.TensorBoard(
+                    log_dir=log_dir, histogram_freq=1
+                ),
+                tf.keras.callbacks.EarlyStopping(
+                    monitor="val_loss",
+                    min_delta=0.00001,
+                    patience=10,
+                    verbose=0,
+                    mode="auto",
+                    baseline=None,
+                    restore_best_weights=True,
+                ),
+            ]
+            lr = keras.optimizers.schedules.ExponentialDecay(
+                0.01, decay_steps=30, decay_rate=0.6, staircase=True
+            )
+            model.compile(
+                optimizer=keras.optimizers.Adam(
+                    learning_rate=lr,
+                    beta_1=0.9,
+                    beta_2=0.999,
+                    epsilon=1e-07,
+                    amsgrad=False,
+                ),
+                loss="binary_crossentropy",
+                metrics=[
+                    keras.metrics.BinaryAccuracy(name="accuracy"),
+                    keras.metrics.Precision(name="precision"),
+                    keras.metrics.Recall(name="recall"),
+                    keras.metrics.AUC(name="auc"),
+                ],
+                # run_eagerly=True,
+            )
+            history = model.fit(
+                X_train,
+                y_train,
+                epochs=epochs,
+                callbacks=callbacks,
+                batch_size=16,
+                validation_data=(X_val, y_val),
+            )
+            histories.append(history)
     ############################################################################
     #  Model evaluation
     ############################################################################
     # Mosly already included into the training procedure.
-    # Stepping back out of the partitions loop
     last_acc = []
     last_val_acc = []
     last_val_prec = []

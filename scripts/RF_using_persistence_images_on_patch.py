74aa89ed5420846f25545a9cf2a6ebbd52375648
Philip Hartout <philip.hartout@protonmail.com>
2020-11-25 17:51:45 +0100

    :sparkles: full revamp of RF script to process persistence landscape

--- scripts/RF_using_persistence_images_on_patch.py
+++ scripts/RF_using_persistence_images_on_patch.py
@@ -1,171 +1,161 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""RF_using_persistence_images_on_patch.py
+"""CNN_using_persistence_images_on_patch.py
 
-The aim of this script is to perform the training of a RF using persistence
-images as a input.
+The aim of this script is to perform the training of a CNN using persistence
+images as a input. This script is inspired from this script:
+BorgwardtLab/ADNI_MRI_Analysis/blob/mixed_CNN/mixed_CNN/run_Sarah.py
+
+To get real time information into the model training and structure, run
+$ tensorboard --logdir logs/fit
+
+once this script has been started.
+
+
+NOTES:
+    - One loaded, the "big" 100x100x3 images aren't that big (>400MB in RAM) so
+      NO GENERATOR NEEDED
+
 """
 
 __author__ = "Philip Hartout"
 __email__ = "philip.hartout@protonmail.com"
 
 import dotenv
-
-import datetime
 import os
 
 import numpy as np
-
-from sklearn.model_selection import train_test_split
-import numpy as np
 import pandas as pd
-
-from sklearn.metrics import fbeta_score, make_scorer
-from sklearn.covariance import EllipticEnvelope
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.ensemble import (
-    ExtraTreesRegressor,
-    IsolationForest,
-    RandomForestClassifier,
-)
-from sklearn.feature_selection import SelectFromModel
-from sklearn.preprocessing import (
-    RobustScaler,
-    QuantileTransformer,
-    PowerTransformer,
-)
-from sklearn.feature_selection import SelectKBest, f_regression, SelectFdr
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
-from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer
-from sklearn.kernel_ridge import KernelRidge
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import (
-    WhiteKernel,
-    ExpSineSquared,
-    RBF,
-    CompoundKernel,
-    ConstantKernel,
-    DotProduct,
-    Exponentiation,
-    Hyperparameter,
-    Kernel,
-    Matern,
-    PairwiseKernel,
-    Product,
-    RationalQuadratic,
-    Sum,
-    WhiteKernel,
-)
-from sklearn.datasets import make_friedman2
-from sklearn.metrics import balanced_accuracy_score
-from sklearn.decomposition import PCA
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_val_score, cross_validate
-from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-
-import shutil
-import utils
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, make_scorer
 
 DOTENV_KEY2VAL = dotenv.dotenv_values()
+N_BINS = 100
+N_LAYERS = 50
+################################################################################
+#  Functions
+################################################################################
+
+persistence_landscape_location = (
+    DOTENV_KEY2VAL["DATA_DIR"] + "/patch_91_persistence_landscapes/"
+)
+partitions_location = DOTENV_KEY2VAL["DATA_DIR"] + "/partitions/"
+diagnosis_json = (
+    DOTENV_KEY2VAL["DATA_DIR"] + "/collected_diagnoses_complete.json"
+)
 
 
+def get_partitions(partitions_location):
+    partition = []
+    labels = []
+    for root, dirs, files in os.walk(partitions_location):
+        for file in files:
+            if file.split("_")[0] == "partition":
+                partition.append(
+                    np.load(
+                        partitions_location + file, allow_pickle=True
+                    ).item()
+                )
+            elif file.split("_")[0] == "labels":
+                labels.append(
+                    np.load(
+                        partitions_location + file, allow_pickle=True
+                    ).item()
+                )
+            else:
+                print(f"File {file} is neither partition nor labels file")
+    return partition, labels
+
+
+################################################################################
+#  Main
+################################################################################
 def main():
-    # This defines where we are in the machine
-    diagnosis_json = (
-        DOTENV_KEY2VAL["DATA_DIR"] + "/collected_diagnoses_complete.json"
-    )
-    persistence_image_location = (
-        DOTENV_KEY2VAL["DATA_DIR"] + "/patch_91_persistence_images/"
-    )
-
     ############################################################################
     #  Data loading and processing
     ############################################################################
+    inits = 1
+    partitions, labels = get_partitions(partitions_location)
+    histories = []
+    for partition, label in zip(partitions, labels):
+        for i in range(inits):
+            # Make sure there aren't the same patients in train and test
+            X_train_lst = []
+            y_train_lst = []
+            for landscape in partition["train"]:
+                X_train_lst.append(
+                    np.load(persistence_landscape_location + landscape + ".npy")
+                )
+                y_train_lst.append(label[landscape])
 
-    # First, we load relevant images (only baseline image for now)
-    (
-        cn_patients,
-        mci_patients,
-        ad_patients,
-    ) = utils.get_all_available_diagnoses(diagnosis_json)
+                X_train, y_train = (
+                    np.stack(X_train_lst, axis=0).reshape(
+                        len(X_train_lst), N_BINS, N_LAYERS, 3
+                    ),
+                    np.vstack(y_train_lst),
+                )
+            X_val_lst = []
+            y_val_lst = []
+            for landscape in partition["validation"]:
+                X_val_lst.append(
+                    np.load(persistence_landscape_location + landscape + ".npy")
+                )
+                y_val_lst.append(label[landscape])
 
-    # For now we only get images for CN and MCI patients
-    images_cn = utils.get_arrays_from_dir(persistence_image_location, cn_patients)
-    images_ad = utils.get_arrays_from_dir(persistence_image_location, ad_patients)
-    # Concatenate both arrays
-    labels = np.array(
-        [0 for i in range(images_cn.shape[0])]  # Labels for CN is 0
-        + [1 for i in range(images_ad.shape[0])]  # Labels for AD is 2
-    ).T
-    images = np.vstack((images_ad, images_cn))
+                X_val, y_val = (
+                    np.stack(X_val_lst, axis=0).reshape(
+                        len(X_val_lst), N_BINS, N_LAYERS, 3
+                    ),
+                    np.vstack(y_val_lst),
+                )
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        images.reshape(images.shape[0], 100 * 100 * 3),
-        labels,
-        test_size=0.2,
-        random_state=42,
-        stratify=labels,
-    )
+            ####################################################################
+            #  Model definition
+            ####################################################################
+            model = RandomForestClassifier()
+            params = {
+                "n_estimators": [300],
+                "criterion": ["gini"],
+                "max_depth": [None],
+                "min_samples_split": [2],
+                "min_samples_leaf": [1],
+                "min_weight_fraction_leaf": [0.0],
+                "max_features": ["auto"],
+                "max_leaf_nodes": [None],
+                "min_impurity_decrease": [0.0],
+                "min_impurity_split": [None],
+                "bootstrap": [True],
+                "oob_score": [False],
+                "n_jobs": [None],
+                "random_state": [42],
+                "verbose": [0],
+                "warm_start": [False],
+                "class_weight": [None],
+                "ccp_alpha": [0.0],
+                "max_samples": [None],
+            }
 
-    ############################################################################
-    #  Model definition
-    ############################################################################
-
-    params = {
-        "n_estimators": [200],
-        "criterion": ["gini"],
-        "max_depth": [None],
-        "min_samples_split": [2],
-        "min_samples_leaf": [1],
-        "min_weight_fraction_leaf": [0.0],
-        "max_features": ["auto"],
-        "max_leaf_nodes": [None],
-        "min_impurity_decrease": [0.0],
-        "min_impurity_split": [None],
-        "bootstrap": [True],
-        "oob_score": [False],
-        "n_jobs": [None],
-        "random_state": [42],
-        "verbose": [0],
-        "warm_start": [False],
-        "class_weight": [None],
-        "ccp_alpha": [0.0],
-        "max_samples": [None],
-    }
-
-    search = GridSearchCV(
-        RandomForestClassifier(),
-        param_grid=params,
-        cv=10,
-        scoring=make_scorer(balanced_accuracy_score),
-        n_jobs=-1,
-        verbose=2,
-    )
-
-    ############################################################################
-    #  Model definition
-    ############################################################################
-
-    search.fit(X_train, y_train)
-
-    ############################################################################
-    #  Model evaluation
-    ############################################################################
-
-    y_pred = search.best_estimator_.predict(X_test)
-
-    print(
-        f"Best model training set cross validated score using RF is"
-        f" {search.best_score_}"
-    )
-    print(f"Best performance is achieved using " f"{search.best_params_}")
-    print(
-        f"Best test set score using RF is {balanced_accuracy_score(y_test, y_pred)}"
-    )
-
+            search = GridSearchCV(
+                RandomForestClassifier(),
+                param_grid=params,
+                cv=10,
+                scoring=make_scorer(accuracy_score),
+                n_jobs=-1,
+            )
+            search.fit(X_train.reshape(len(X_train),-1), y_train.ravel())
+            y_val_pred = search.best_estimator_.predict(X_val.reshape(len(
+                X_val),-1))
+            print(
+                f"Best model training set cross validated score using RF is"
+                f" {search.best_score_}"
+            )
+            print(
+                f"Best performance is achieved using " f"{search.best_params_}")
+            print(
+                f"Best test set score using RF is "
+                f"{accuracy_score(y_val, y_val_pred)}"
+            )
 
 if __name__ == "__main__":
     main()

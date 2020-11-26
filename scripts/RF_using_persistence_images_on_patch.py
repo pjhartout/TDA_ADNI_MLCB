4c50d083a303740100c40b99ef3802a20bb991b2
Philip Hartout <philip.hartout@protonmail.com>
2020-11-26 21:05:51 +0100

    :art: formatting issues

--- scripts/RF_using_persistence_images_on_patch.py
+++ scripts/RF_using_persistence_images_on_patch.py
@@ -28,6 +28,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, make_scorer
 
 DOTENV_KEY2VAL = dotenv.dotenv_values()
